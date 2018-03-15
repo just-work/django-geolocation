717db9c9263a1c5eecd7d82542ac0e3ed5deda17
hale <d.zakharov@just-work.org>
2018-03-15 14:18:36 +0500

    В setup.py указана старая версия

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-geolocation',
-    version='1.0',
+    version='1.3.1',
     packages=['geolocation'],
     include_package_data=True,
     license='BSD License',  # example license
